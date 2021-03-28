28867dfa9e65f52e741922c151bc8563390f3a66
areitools <80171286+areitools@users.noreply.github.com>
2021-03-28 11:21:58 +0100

    Placeholder FILES - 28th March 2021

---
+++ AreiTools W10.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+title AreiTools W10
+echo --------------------------------------------
+echo AreiTools is not currently ready to be used
+echo It will be released on 3 April 2021
+echo --------------------------------------------
+echo.
+echo Press any key to close
+timeout 99999 > nul
